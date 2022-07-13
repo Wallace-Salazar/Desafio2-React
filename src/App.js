3f2faa165fca57144ddea44c65684d1771a021b2
Wallace Oliveira <104787895+Wallace-Salazar@users.noreply.github.com>
2022-07-13 00:07:31 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          "Um dos meus dias mais produtivos foi quando eu joguei fora 1000
+          linhas de código."
+        </h1>
+        <p>– Ken Thompson</p>
+      </div>
+    );
+  }
+}
+
+export default App;
